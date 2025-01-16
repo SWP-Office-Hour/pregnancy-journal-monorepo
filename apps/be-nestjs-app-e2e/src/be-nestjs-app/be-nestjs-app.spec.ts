5eed5711c7253ef7da6cdeb3734d7fce87de9f2c
ThanhTNV <thanhtran190304@gmail.com>
2025-01-16 14:39:44 +0700

    fix: fix app e2e testing api

--- apps/be-nestjs-app-e2e/src/be-nestjs-app/be-nestjs-app.spec.ts
+++ apps/be-nestjs-app-e2e/src/be-nestjs-app/be-nestjs-app.spec.ts
@@ -1,10 +1,27 @@
-import axios from 'axios';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../../../be-nestjs-app/src/app/app.module';
+import request from 'supertest';
 
 describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+  let app: INestApplication;
 
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule], // Replace with your app's root module
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should return a message', async () => {
+    const res = await request(app.getHttpServer()).get('/');
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.body).toEqual({ message: 'Hello API' });
   });
 });
