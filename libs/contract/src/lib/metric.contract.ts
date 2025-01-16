8f6c5ef3b5b3deb5c8f36970f2d3178cad20d695
phatcanlez <phatcanlez@gmail.com>
2025-01-16 10:21:46 +0700

    feat: add zod of tag, membership, metric, dashboard

--- libs/contract/src/lib/metric.contract.ts
+++ libs/contract/src/lib/metric.contract.ts
@@ -1,7 +1,6 @@
 import {z} from 'zod';
 import {initContract} from '@ts-rest/core';
 import { tagResSchema } from './tag.contract';
-import { statusSchema } from './enum.contract';
 
 const boundSchema = z.object({
   id: z.string(),
@@ -10,24 +9,10 @@ const boundSchema = z.object({
   upper: z.number(),
 });
 
-export const metricResSchema = z.object({
+const metricResSchema = z.object({
   id: z.string(),
   title: z.string(),
   measure: z.string(),
   bound: z.array(boundSchema),
-  upper_bound_msg: z.string(),
-  lower_bound_msg: z.string(),
   tags: z.array(tagResSchema),
-  status: statusSchema,
 });
-
-export const metricUserResSchema = z.object({
-  id: z.string(),
-  title: z.string(),
-  measure: z.string(),
-  upper_bound_msg: z.string(),
-  lower_bound_msg: z.string(),
-  bound: boundSchema,
-});
-
-const metricGetAllResSchema = z.array(metricResSchema);
