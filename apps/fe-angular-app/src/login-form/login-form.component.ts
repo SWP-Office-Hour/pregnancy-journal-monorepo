f401258dfe6a55ddfcd41a6580ab9355f079fceb
HungLT <hungltse184143@fpt.edu.vn>
2025-01-17 12:33:43 +0700

    fix: update package.json

--- apps/fe-angular-app/src/login-form/login-form.component.ts
+++ apps/fe-angular-app/src/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -21,8 +21,9 @@ import {
   TuiSwitch,
   TuiTooltip,
   TuiPassword,
-} from '@taiga-ui/kit';
+} from '@taiga-ui/npm i @taiga-ui/{cdk,core,kit,icons}';
 import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
+import { AuthGoogleService } from '../services/auth-google.service';
 
 @Component({
   selector: 'formLogin',
@@ -52,8 +53,13 @@ import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class LoginFormComponent {
-  protected readonly form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-  });
+  // protected readonly form = new FormGroup({
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl('', [Validators.required]),
+  // });
+  private authService = inject(AuthGoogleService);
+
+  signInWithGoogle() {
+    this.authService.login();
+  }
 }
