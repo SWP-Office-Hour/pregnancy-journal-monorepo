20113876f6d33077b5fcc21a823b286c5e01a7c1
nnh53 <nnh53.work@gmail.com>
2025-01-17 13:31:20 +0700

    fix: fix dep and package.json

--- apps/fe-angular-app/src/main.ts
+++ apps/fe-angular-app/src/main.ts
@@ -1,6 +1,9 @@
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {NG_EVENT_PLUGINS} from '@taiga-ui/event-plugins';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
