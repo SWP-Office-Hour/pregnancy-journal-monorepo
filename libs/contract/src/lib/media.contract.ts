60caab0a6bd375b923429283a6df44188b6c5ebe
phatcanlez <phatcanlez@gmail.com>
2025-01-17 11:48:17 +0700

    feat: done all zod and contract at 17/1/2025

--- libs/contract/src/lib/media.contract.ts
+++ libs/contract/src/lib/media.contract.ts
@@ -1,7 +1,46 @@
-import {z} from 'zod';
-import {initContract} from '@ts-rest/core';
+import { z } from 'zod';
+import { initContract } from '@ts-rest/core';
 
 export const mediaResSchema = z.object({
   id: z.string(),
   url: z.string(),
+});
+
+const c = initContract();
+
+export const mediaContract = c.router({
+  updateFile: {
+    method: 'POST',
+    path: '/file',
+    contentType: 'multipart/form-data', // <- Only difference
+    body: c.type<{ thumbnail: File }>(), // <- Use File type in here
+    responses: {
+      200: mediaResSchema,
+      400: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  getFile: {
+    method: 'GET',
+    path: '/file/:filename',
+    responses: {
+      200: mediaResSchema,
+      400: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  deleteFile: {
+    method: 'DELETE',
+    path: '/file/:filename',
+    responses: {
+      200: z.object({
+        message: z.string(),
+      }),
+      400: z.object({
+        message: z.string(),
+      }),
+    },
+  },
 });
