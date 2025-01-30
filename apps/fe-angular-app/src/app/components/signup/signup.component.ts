383560c0895167d7e84e7821caf0eae47bdc511b
HungLT <hungltse184143@fpt.edu.vn>
2025-01-31 01:51:41 +0700

    feat: create signup component

---
+++ apps/fe-angular-app/src/app/components/signup/signup.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AuthGoogleService } from '../../services/auth-google.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
+import { TuiAppearance, TuiButton, TuiIcon, TuiTextfield, TuiTitle } from '@taiga-ui/core';
+import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
+import { TuiPassword, TuiTooltip } from '@taiga-ui/kit';
+import { UsersService } from '../../services/users.service';
+
+@Component({
+  selector: 'app-signup.component',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    TuiAppearance,
+    TuiButton,
+    TuiCardLarge,
+    TuiForm,
+    TuiHeader,
+    TuiIcon,
+    TuiTextfield,
+    TuiTitle,
+    TuiTooltip,
+    TuiPassword,
+    NgOptimizedImage,
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponentComponent {
+  protected readonly loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+  protected readonly NgOptimizedImage = NgOptimizedImage;
+  private authService = inject(AuthGoogleService);
+  private userService: UsersService = inject(UsersService);
+
+  signInWithGoogle() {
+    this.authService.login();
+  }
+  signup() {
+    // this.userService.login();
+  }
+}
