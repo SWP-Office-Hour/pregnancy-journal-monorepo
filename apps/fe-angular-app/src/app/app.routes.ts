4abf7bea6d24b43d695c3528638cbaddf78fc177
HungLT <hungltse184143@fpt.edu.vn>
2025-01-31 01:46:51 +0700

    feat: fix login component

--- apps/fe-angular-app/src/app/app.routes.ts
+++ apps/fe-angular-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignupComponentComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupComponentComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
