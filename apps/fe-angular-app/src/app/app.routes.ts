523fa5215c2e928e9b8b84643297a546e8b87648
nnh53 <nnh53.work@gmail.com>
2025-01-31 19:33:12 +0700

    feat: pregnancy tracking screen, including tracking form, tracking stepper and tracking service

--- apps/fe-angular-app/src/app/app.routes.ts
+++ apps/fe-angular-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PregnancyTrackingComponent } from './pregnancy-tracking/pregnancy-tracking.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'pregnancy-tracking', component: PregnancyTrackingComponent },
 ];
---
+++ apps/fe-angular-app/src/app/pregnancy-tracking/pregnancy-tracking.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { metricRes, pregnancyResponse, pregnancyUpdateRequest } from '@pregnancy-journal-monorepo/contract';
+import { hospitals, metrics, pregnancyData } from './pregnancy-tracking.mock-api';
+
+@Injectable({ providedIn: 'root' })
+export class PregnancyTrackingService {
+  protected pregnancyData: pregnancyResponse = pregnancyData;
+  protected metrics: metricRes = metrics;
+  protected hospitals: string[] = hospitals;
+
+  updateData(data: pregnancyUpdateRequest) {
+    console.log(data);
+  }
+
+  getPregnancyData() {
+    return this.pregnancyData;
+  }
+
+  getMetricsForUsers() {
+    return this.metrics;
+  }
+
+  getHospitalList() {
+    return this.hospitals;
+  }
+}
