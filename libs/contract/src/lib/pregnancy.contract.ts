f988ff00c373af54817d31f839b5c2273f04cde9
phatcanlez <phatcanlez@gmail.com>
2025-02-03 10:00:14 +0700

    feat: api metric

--- libs/contract/src/lib/pregnancy.contract.ts
+++ libs/contract/src/lib/pregnancy.contract.ts
@@ -1,15 +1,12 @@
 import { z } from 'zod';
 import { initContract } from '@ts-rest/core';
-import { metricUserResSchema } from './metric.contract';
-
-export type pregnancyResponse = z.infer<typeof pregnancyResSchema>;
-export type pregnancyUpdateRequest = z.infer<typeof pregnancyUpdateReqSchema>;
+import { metricResSchema } from './metric.contract';
 
 export const metricValueSchema = z.array(
   z.object({
     id: z.string(),
     value: z.number(),
-    metric: metricUserResSchema,
+    metric: metricResSchema,
   }),
 );
 
@@ -21,50 +18,17 @@ const pregnancyResSchema = z.array(
     nextVisitDate: z.date(),
     visitDoctorDate: z.date(),
     hospital: z.string(),
-    data: metricValueSchema,
+    data: z.array(metricValueSchema),
   }),
 );
-
-const pregnancyUpdateReqSchema = z.object({
-  id: z.string(),
-  week: z.number().optional(),
-  expectedBirthDate: z.date().optional(),
-  nextVisitDate: z.date().optional(),
-  visitDoctorDate: z.date().optional(),
-  hospital: z.string().optional(),
-  data: metricValueSchema.optional(),
-});
 
 const c = initContract();
 
 export const pregnancyContract = c.router({
-  getAllPregnancies: {
-    method: 'GET',
-    path: '/pregnancy',
-    description: 'Get all pregnancies',
-    responses: {
-      200: pregnancyResSchema,
-      400: z.object({
-        message: z.string(),
-      }),
-    },
-  },
   getPregnancyById: {
     method: 'GET',
     path: '/pregnancy/:id',
     description: 'Get pregnancy by user id',
-    responses: {
-      200: pregnancyResSchema,
-      400: z.object({
-        message: z.string(),
-      }),
-    },
-  },
-  updatePregnancy: {
-    method: 'PUT',
-    path: '/pregnancy/:id',
-    description: 'Update pregnancy by user id',
-    body: pregnancyUpdateReqSchema,
     responses: {
       200: pregnancyResSchema,
       400: z.object({
