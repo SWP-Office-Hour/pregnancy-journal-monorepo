7c1c156f1df13feedb038a853c5fcc5b94e033e4
ThanhTNV <thanhtran190304@gmail.com>
2025-02-04 14:21:37 +0700

    feat: upload img finish

--- apps/fe-angular-app/src/app/pregnancy-tracking/pregnancy-tracking-file-upload/file-upload.component.ts
+++ apps/fe-angular-app/src/app/pregnancy-tracking/pregnancy-tracking-file-upload/file-upload.component.ts
@@ -1,7 +1,6 @@
-import { Component, output, ViewChild } from '@angular/core';
+import { Component, ElementRef, output, signal, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TuiFileLike, TuiFiles } from '@taiga-ui/kit';
-import { map, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-file-upload',
@@ -10,41 +9,33 @@ import { map, Subject } from 'rxjs';
   styleUrl: './file-upload.component.css',
 })
 export class FileUploadComponent {
-  uploadFiles = output<TuiFileLike>();
-  @ViewChild('imgPreview') imgPreview!: HTMLDivElement;
+  uploadFiles = output<TuiFileLike[]>();
+  uploadFilesSrc = output<string[]>();
 
-  protected files: TuiFileLike[] = [];
+  currFile = signal<TuiFileLike | null>(null);
+
+  protected imgSrcListSignal = signal<string[]>([]);
+
+  protected filesSignal = signal<TuiFileLike[]>([]);
   protected readonly control = new FormControl<TuiFileLike | null>(null, [Validators.required]);
-  // protected readonly failedFiles$ = new Subject<TuiFileLike | null>();
-  // protected readonly loadingFiles$ = new Subject<TuiFileLike | null>();
-  // protected readonly loadedFiles$ = this.control.valueChanges.pipe(
-  //   map((file: TuiFileLike | null) => this.processFile(file)),
-  // );
 
   protected removeFile(): void {
-    this.control.setValue(null);
+    //
   }
 
-  // protected processFile(file: TuiFileLike | null): TuiFileLike | null {
-  //   this.loadingFiles$.next(file);
-  //   if (!file) {
-  //     this.failedFiles$.next(null);
-  //     this.failedFiles$.next(file);
-  //     return null;
-  //   } else {
-  //     this.loadingFiles$.next(null);
-  //     this.failedFiles$.next(null);
-  //     return file;
-  //   }
-  // }
-
   protected onFileChange(): void {
-    // this.loadedFiles$.subscribe({
-    //   next: (file) => {
-    //     if (file) {
-    //       this.uploadFiles.emit(file);
-    //     }
-    //   },
-    // });
+    const reader = new FileReader();
+    this.control.valueChanges.subscribe((file) => {
+      if (file !== this.currFile()) {
+        this.filesSignal().push(file!);
+        reader.readAsDataURL(file as File);
+        reader.onload = () => {
+          this.imgSrcListSignal().push(reader.result as string);
+        };
+        this.currFile.set(file);
+        this.uploadFiles.emit(this.filesSignal());
+        this.uploadFilesSrc.emit(this.imgSrcListSignal());
+      }
+    });
   }
 }
