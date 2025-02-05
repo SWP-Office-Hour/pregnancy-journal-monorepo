42702c120deee71e8220d6ee8dd6744cf35e676e
phatcanlez <phatcanlez@gmail.com>
2025-02-05 09:12:36 +0700

    fix: package, add media query

--- apps/be-nestjs-app/src/app/file/file.module.ts
+++ apps/be-nestjs-app/src/app/file/file.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { FileService } from './file.service';
 
 @Module({
-  imports: [
-    MulterModule.register({
-      dest: './uploads',
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          cb(null, file.originalname);
-        },
-      }),
-    }),
-  ],
+  imports: [],
   providers: [FileService],
 })
 export class FileModule {}
--- apps/be-nestjs-app/src/app/media/media.module.ts
+++ apps/be-nestjs-app/src/app/media/media.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { DatabaseService } from '../database/database.service';
+import { FileService } from '../file/file.service';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  ],
   controllers: [MediaController],
-  providers: [MediaService, DatabaseService],
+  providers: [MediaService, DatabaseService, FileService],
 })
 export class MediaModule {}
