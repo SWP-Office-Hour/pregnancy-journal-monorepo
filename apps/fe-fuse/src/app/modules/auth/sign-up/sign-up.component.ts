8c424ce74241b86ed353273187ddf97a96810b46
nnh53 <nnh53.work@gmail.com>
2025-02-07 16:39:03 +0700

    feat: this is the example app

--- apps/fe-fuse/src/app/modules/auth/sign-up/sign-up.component.ts
+++ apps/fe-fuse/src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import {
-  FormsModule,
-  NgForm,
-  ReactiveFormsModule,
-  UntypedFormBuilder,
-  UntypedFormGroup,
-  Validators,
+    FormsModule,
+    NgForm,
+    ReactiveFormsModule,
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -19,102 +19,102 @@ import { FuseAlertComponent, FuseAlertType } from '@fuse/components/alert';
 import { AuthService } from 'app/core/auth/auth.service';
 
 @Component({
-  selector: 'auth-sign-up',
-  templateUrl: './sign-up.component.html',
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations,
-  standalone: true,
-  imports: [
-    RouterLink,
-    FuseAlertComponent,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCheckboxModule,
-    MatProgressSpinnerModule,
-  ],
+    selector: 'auth-sign-up',
+    templateUrl: './sign-up.component.html',
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations,
+    standalone: true,
+    imports: [
+        RouterLink,
+        FuseAlertComponent,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCheckboxModule,
+        MatProgressSpinnerModule,
+    ],
 })
 export class AuthSignUpComponent implements OnInit {
-  @ViewChild('signUpNgForm') signUpNgForm: NgForm;
+    @ViewChild('signUpNgForm') signUpNgForm: NgForm;
 
-  alert: { type: FuseAlertType; message: string } = {
-    type: 'success',
-    message: '',
-  };
-  signUpForm: UntypedFormGroup;
-  showAlert: boolean = false;
+    alert: { type: FuseAlertType; message: string } = {
+        type: 'success',
+        message: '',
+    };
+    signUpForm: UntypedFormGroup;
+    showAlert: boolean = false;
 
-  /**
-   * Constructor
-   */
-  constructor(
-    private _authService: AuthService,
-    private _formBuilder: UntypedFormBuilder,
-    private _router: Router,
-  ) {}
+    /**
+     * Constructor
+     */
+    constructor(
+        private _authService: AuthService,
+        private _formBuilder: UntypedFormBuilder,
+        private _router: Router
+    ) {}
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Lifecycle hooks
-  // -----------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
 
-  /**
-   * On init
-   */
-  ngOnInit(): void {
-    // Create the form
-    this.signUpForm = this._formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      company: [''],
-      agreements: ['', Validators.requiredTrue],
-    });
-  }
-
-  // -----------------------------------------------------------------------------------------------------
-  // @ Public methods
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * Sign up
-   */
-  signUp(): void {
-    // Do nothing if the form is invalid
-    if (this.signUpForm.invalid) {
-      return;
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        // Create the form
+        this.signUpForm = this._formBuilder.group({
+            name: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+            company: [''],
+            agreements: ['', Validators.requiredTrue],
+        });
     }
 
-    // Disable the form
-    this.signUpForm.disable();
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
 
-    // Hide the alert
-    this.showAlert = false;
+    /**
+     * Sign up
+     */
+    signUp(): void {
+        // Do nothing if the form is invalid
+        if (this.signUpForm.invalid) {
+            return;
+        }
 
-    // Sign up
-    this._authService.signUp(this.signUpForm.value).subscribe(
-      (response) => {
-        // Navigate to the confirmation required page
-        this._router.navigateByUrl('/confirmation-required');
-      },
-      (response) => {
-        // Re-enable the form
-        this.signUpForm.enable();
+        // Disable the form
+        this.signUpForm.disable();
 
-        // Reset the form
-        this.signUpNgForm.resetForm();
+        // Hide the alert
+        this.showAlert = false;
 
-        // Set the alert
-        this.alert = {
-          type: 'error',
-          message: 'Something went wrong, please try again.',
-        };
+        // Sign up
+        this._authService.signUp(this.signUpForm.value).subscribe(
+            (response) => {
+                // Navigate to the confirmation required page
+                this._router.navigateByUrl('/confirmation-required');
+            },
+            (response) => {
+                // Re-enable the form
+                this.signUpForm.enable();
 
-        // Show the alert
-        this.showAlert = true;
-      },
-    );
-  }
+                // Reset the form
+                this.signUpNgForm.resetForm();
+
+                // Set the alert
+                this.alert = {
+                    type: 'error',
+                    message: 'Something went wrong, please try again.',
+                };
+
+                // Show the alert
+                this.showAlert = true;
+            }
+        );
+    }
 }
