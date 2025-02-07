03f7a273a10f1cc0c2be19bc10455454df38ea5b
ThanhTNV <thanhtran190304@gmail.com>
2025-02-07 19:28:06 +0700

    fix: add layout component to screen

--- apps/fe-fuse/src/app/pregnancy-record/pregnancy-record.component.ts
+++ apps/fe-fuse/src/app/pregnancy-record/pregnancy-record.component.ts
@@ -1,10 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsFieldsComponent } from '../modules/admin/ui/forms/fields/fields.component';
 
 @Component({
   selector: 'app-pregnancy-record',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    FormsFieldsComponent,
+  ],
   templateUrl: './pregnancy-record.component.html',
-  styleUrl: './pregnancy-record.component.css',
 })
 export class PregnancyRecordComponent {}
