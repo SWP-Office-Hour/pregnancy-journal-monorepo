7b80981cfd251d40223e8d26205d745c7ecf4b08
phatcanlez <phatcanlez@gmail.com>
2025-02-08 21:40:22 +0700

    feat: add time calculate week pregnancy

---
+++ apps/be-nestjs-app/src/app/utils/time/timeUtils.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TimeUtilsService {
+  calculatePregnancyWeeks({ expectedBirthDate, visitDate }: { expectedBirthDate: Date; visitDate: Date }) {
+    const timeDiff = expectedBirthDate.getTime() - visitDate.getTime();
+    const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    // Tính tuần thai
+    const pregnancyWeek = 40 - daysRemaining / 7;
+
+    return Number(pregnancyWeek.toFixed(1));
+  }
+}
