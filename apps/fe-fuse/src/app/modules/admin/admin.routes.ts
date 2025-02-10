e73984a86d60ef2a5ba7e6811e473dde1af91978
ThanhTNV <thanhtran190304@gmail.com>
2025-02-10 11:04:29 +0700

    feat: create blog screen for admin

--- apps/fe-fuse/src/app/modules/admin/admin.routes.ts
+++ apps/fe-fuse/src/app/modules/admin/admin.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { CreateBlogComponent } from './create-blog/create-blog.component';
 
 export default [
   {
     path: '',
     component: AdminComponent,
+  },
+  {
+    path: 'blogs',
+    component: CreateBlogComponent,
   },
 ] as Routes;
