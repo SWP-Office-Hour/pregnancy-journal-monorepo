73f2c6e273f35001a1a9bc3a2df7b87034df8adc
phatcanlez <phatcanlez@gmail.com>
2025-02-11 21:56:36 +0700

    fix: remove refresh token api

--- apps/be-nestjs-app/src/app/users/users.controller.ts
+++ apps/be-nestjs-app/src/app/users/users.controller.ts
@@ -66,26 +66,26 @@ export class UsersController {
     });
   }
 
-  @UseGuards(RefreshTokenAuthGuard)
-  @TsRestHandler(authContract.refreshToken)
-  async handleRefreshToken(@Body() body: TokenRequest, @Req() req: RequestWithJWT) {
-    return tsRestHandler(authContract.refreshToken, async () => {
-      const { user_id } = req.decoded_refresh_token;
-      const { refresh_token } = body;
-      const refresh_token_id = await this.usersService.checkRefreshToken({
-        user_id,
-        refresh_token,
-      });
-      if (!refresh_token_id) {
-        throw new UnauthorizedException('Unauthorized Refresh Token');
-      }
-      const users = await this.usersService.refreshToken({
-        refresh_token_id,
-        user_id,
-      });
-      return { status: 200, body: users };
-    });
-  }
+  // @UseGuards(RefreshTokenAuthGuard)
+  // @TsRestHandler(authContract.refreshToken)
+  // async handleRefreshToken(@Body() body: TokenRequest, @Req() req: RequestWithJWT) {
+  //   return tsRestHandler(authContract.refreshToken, async () => {
+  //     const { user_id } = req.decoded_refresh_token;
+  //     const { refresh_token } = body;
+  //     const refresh_token_id = await this.usersService.checkRefreshToken({
+  //       user_id,
+  //       refresh_token,
+  //     });
+  //     if (!refresh_token_id) {
+  //       throw new UnauthorizedException('Unauthorized Refresh Token');
+  //     }
+  //     const users = await this.usersService.refreshToken({
+  //       refresh_token_id,
+  //       user_id,
+  //     });
+  //     return { status: 200, body: users };
+  //   });
+  // }
 
   @TsRestHandler(userContract.create)
   async handleCreate(@Body() body: UserCreateRequest) {
