b0184fe1b1ede00a035590ef42b14814331ffeee
nnh53 <nnh53.work@gmail.com>
2025-02-11 14:12:32 +0700

    feat: fix environment fe

--- apps/fe-fuse/src/environments/environment.ts
+++ apps/fe-fuse/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://pregnancy-journal-swp-gduen.ondigitalocean.app/api',
   clientId: '616565294967-v2e8j0mi27a6ppbpt00mj7ge2dc2ninj.apps.googleusercontent.com',
 };
