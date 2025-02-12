106cd24b110b3996aeab39ea7fe068150b65c367
ThanhTNV <thanhtran190304@gmail.com>
2025-02-12 08:07:03 +0700

    fix: call api by env variable

--- apps/fe-fuse/src/environments/environment.development.ts
+++ apps/fe-fuse/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3000/',
   clientId: '616565294967-v2e8j0mi27a6ppbpt00mj7ge2dc2ninj.apps.googleusercontent.com',
 };
