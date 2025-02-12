8d1561abccd92aaf5d09ecb74216bb5576da87e1
ThanhTNV <thanhtran190304@gmail.com>
2025-02-12 20:51:22 +0700

    feat: modify record component, record service, remove api service

--- apps/fe-fuse/src/app/modules/customer/pregnancy-record/form/record-form.component.ts
+++ apps/fe-fuse/src/app/modules/customer/pregnancy-record/form/record-form.component.ts
@@ -1,19 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input, OnInit, signal } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MediaRes } from '@pregnancy-journal-monorepo/contract';
+import { Hospital, MediaRes, MetricRes } from '@pregnancy-journal-monorepo/contract';
 import { pregnancyGetRes } from '../../../../mock-api/pages/pregnancy/pregnancy.mock-api';
 import { FileUploadComponent } from '../../common/file-upload/file-upload.component';
-import { addControlToForm, formControls } from '../../common/form/formUtils';
 import { ImagePreviewComponent } from '../../common/image-preview/image-preview.component';
-import { PregnancyRecordApiService } from '../service/pregnancy-record.api.service';
-import { PregnancyRecordSignalService } from '../service/pregnancy-record.signal.service';
+import { PregnancyRecordService } from '../pregnancy-record.service';
 
 @Component({
   selector: 'record-form',
@@ -35,74 +33,66 @@ import { PregnancyRecordSignalService } from '../service/pregnancy-record.signal
   templateUrl: './record-form.component.html',
   styleUrl: './record-form.component.css',
 })
-export class RecordFormComponent implements OnInit {
-  signalService: PregnancyRecordSignalService = inject(PregnancyRecordSignalService);
-  apiService: PregnancyRecordApiService = inject(PregnancyRecordApiService);
+export class RecordFormComponent {
   @Input() data: pregnancyGetRes;
-  protected imgSrcListSignal = this.signalService.MediaSrc;
-  protected pregnancyForm: FormGroup;
-  protected formControls = signal<formControls>([]);
+  protected images: MediaRes[];
+  protected recordForm: FormGroup;
+  protected hospitals: Hospital[];
+  protected metrics: MetricRes[];
 
-  ngOnInit() {
-    this.initForm();
+  constructor(
+    private _recordService: PregnancyRecordService,
+    private _formBuilder: FormBuilder,
+  ) {
+    this.recordForm = this._formBuilder.group(
+      {
+        visit_doctor_date: new Date(),
+        next_visit_doctor_date: new Date(),
+        hospital: '',
+        doctor_name: '',
+        metrics: this._formBuilder.array([]),
+      },
+      {
+        validators: [Validators.required],
+      },
+    );
+    this._recordService.getHospital().subscribe((hospitals) => {
+      this.hospitals = hospitals;
+    });
+    this._recordService.getMetrics().subscribe((metrics) => {
+      this.metrics = metrics;
+      this.metrics.forEach(() => {
+        this.metricsFormArray.push(this._formBuilder.control(0));
+      });
+    });
+    this.images = this._recordService.getMediaSrc();
   }
 
-  initForm() {
-    this.pregnancyForm = new FormGroup({});
-    // this.addControlToForm('visit_doctor_date', new Date(), 'Date', 'Ngày đi khám bác sĩ', this.pregnancyForm);
-    addControlToForm({
-      controlName: 'visit_doctor_date',
-      controlValue: new Date(),
-      controlType: 'Date',
-      controlLabel: 'Ngày đi khám bác sĩ',
-      formGroup: this.pregnancyForm,
-      formControls: this.formControls,
-    });
-    // this.addControlToForm('next_visit_doctor_date', new Date(), 'Date', 'Ngày đi khám tiếp theo', this.pregnancyForm);
-    addControlToForm({
-      controlName: 'next_visit_doctor_date',
-      controlValue: new Date(),
-      controlType: 'Date',
-      controlLabel: 'Ngày đi khám tiếp theo',
-      formGroup: this.pregnancyForm,
-      formControls: this.formControls,
-    });
-    this.apiService.getHospitalList().subscribe((hospitals) => {
-      // this.addControlToForm('hospital', '', 'Select', 'Bệnh viện', hospitals, this.pregnancyForm);
-      addControlToForm({
-        controlName: 'hospital',
-        controlValue: '',
-        controlType: 'Select',
-        controlLabel: 'Bệnh viện',
-        selectItems: hospitals,
-        formGroup: this.pregnancyForm,
-        formControls: this.formControls,
-      });
-    });
-    this.apiService.getMetrics().subscribe((metrics) => {
-      metrics.forEach((metric) => {
-        // this.addControlToForm(metric.id, 0, 'Number', metric.title, undefined, this.pregnancyForm);
-        addControlToForm({
-          controlName: metric.id,
-          controlValue: 0,
-          controlType: 'Number',
-          controlLabel: metric.title,
-          formGroup: this.pregnancyForm,
-          formControls: this.formControls,
-        });
-      });
-    });
+  get metricsFormArray() {
+    return this.recordForm.get('metrics') as FormArray;
   }
 
   submitForm() {
-    this.signalService.submit(this.pregnancyForm.value);
+    const data = this.metricsFormArray.controls.map((control, index) => ({
+      metric_id: this.metrics[index].id,
+      value: control.value as number,
+    }));
+    const { visit_doctor_date, next_visit_doctor_date, doctor_name, hospital } = this.recordForm.value;
+    const formData = {
+      hospital_id: hospital,
+      doctor_name,
+      visit_doctor_date: new Date(visit_doctor_date).toISOString(),
+      next_visit_doctor_date: new Date(next_visit_doctor_date).toISOString(),
+      data,
+    };
+    this._recordService.submit(formData);
   }
 
   deleteImg(id: string) {
-    this.signalService.deleteImage(id);
+    this._recordService.deleteImage(id);
   }
 
   insertImg(img: MediaRes) {
-    this.signalService.addImage(img);
+    this._recordService.addImage(img);
   }
 }
