6c9855bcc712a036b19a14d0dac1f289ceec1dd7
phatcanlez <phatcanlez@gmail.com>
2025-02-15 08:32:55 +0700

    fix: strict payos

--- apps/be-nestjs-app/src/app/payos/payos.service.ts
+++ apps/be-nestjs-app/src/app/payos/payos.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import PayOS from '@payos/node';
-import { CheckoutRequestType } from '@payos/node/lib/type';
 
 @Injectable()
 export class PayosService {
-  constructor(private configService: ConfigService) {}
-  async createLinkPayment(payosCreateRequest: CheckoutRequestType) {
-    const payOS = new PayOS(process.env.PAYOS_CLIENT_ID, process.env.PAYOS_API_KEY, process.env.PAYOS_CHECKSUM_KEY);
-    console.log(this.configService.get<string>('PAYOS_CLIENT_ID'));
-    // console.log(payOS)
-    // return await payOS.createPaymentLink(payosCreateRequest);
-  }
+  // constructor(private configService: ConfigService) {}
+  // async createLinkPayment(payosCreateRequest: CheckoutRequestType) {
+  //   const payOS = new PayOS(process.env.PAYOS_CLIENT_ID, process.env.PAYOS_API_KEY, process.env.PAYOS_CHECKSUM_KEY);
+  //   console.log(this.configService.get<string>('PAYOS_CLIENT_ID'));
+  //   // console.log(payOS)
+  //   // return await payOS.createPaymentLink(payosCreateRequest);
+  // }
 }
