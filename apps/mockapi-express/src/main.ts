82dc15fe7b80dc0578b105960135a4d685c7b8cb
nnh53 <nnh53.work@gmail.com>
2025-02-16 22:10:27 +0700

    feat: add mockapi express app and remove unused package and do some stuff in fe

---
+++ apps/mockapi-express/src/main.ts
@@ -0,0 +1,31 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import cors from 'cors';
+import express from 'express';
+import * as path from 'path';
+import { healthMetricListMockData } from './health-metric.mockapi';
+
+const app = express();
+const corsOptions = {
+  origin: ['http://localhost:4222'],
+};
+app.use(cors(corsOptions));
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to mockapi-express!' });
+});
+
+app.get('/api/metrics', (req, res) => {
+  res.send(healthMetricListMockData);
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
