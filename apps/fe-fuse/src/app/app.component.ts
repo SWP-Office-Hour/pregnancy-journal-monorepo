a5da21eff96ae0e392924240f776f259cd18a945
nnh53 <nnh53.work@gmail.com>
2025-02-16 10:33:23 +0700

    feat: finally can call but wtf 2 day

--- apps/fe-fuse/src/app/app.component.ts
+++ apps/fe-fuse/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserSearchComponent } from './user-search/user-search.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, UserSearchComponent],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   /**
--- apps/fe-fuse/src/app/modules/health-metric/health-metric.route.ts
+++ apps/fe-fuse/src/app/modules/health-metric/health-metric.route.ts
@@ -1,23 +1,22 @@
 import { Routes } from '@angular/router';
 import { HealthMetricTableComponent } from './health-metric-table/health-metric-table.component';
-import { HealthMetricComponent } from './health-metric.component';
 
 export default [
   {
     path: '',
-    component: HealthMetricComponent,
-    children: [
-      {
-        path: '/table',
-        component: HealthMetricTableComponent,
-        // resolve: {
-        //   brands: () => inject(InventoryService).getBrands(),
-        //   categories: () => inject(InventoryService).getCategories(),
-        //   products: () => inject(InventoryService).getProducts(),
-        //   tags: () => inject(InventoryService).getTags(),
-        //   vendors: () => inject(InventoryService).getVendors(),
-        // },
-      },
-    ],
+    component: HealthMetricTableComponent,
+    // children: [
+    //   {
+    //     path: '/table',
+    //     component: ,
+    //     // resolve: {
+    //     //   brands: () => inject(InventoryService).getBrands(),
+    //     //   categories: () => inject(InventoryService).getCategories(),
+    //     //   products: () => inject(InventoryService).getProducts(),
+    //     //   tags: () => inject(InventoryService).getTags(),
+    //     //   vendors: () => inject(InventoryService).getVendors(),
+    //     // },
+    //   },
+    // ],
   },
 ] as Routes;
