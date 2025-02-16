28cc5ecb2e67a0281e3c8746b9b54e44e1536f5b
nnh53 <nnh53.work@gmail.com>
2025-02-16 10:10:30 +0700

    feat: example resource

--- apps/fe-fuse/src/app/app.component.ts
+++ apps/fe-fuse/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserSearchComponent],
 })
 export class AppComponent {
   /**
---
+++ apps/fe-fuse/src/app/user-search/user-search.component.ts
@@ -0,0 +1,52 @@
+import { Component, resource, signal } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { API_URL } from './config';
+import { User } from './model';
+
+@Component({
+  selector: 'app-user-search',
+  imports: [MatProgressBarModule],
+  template: `
+    <fieldset>
+      <legend>Users Search</legend>
+      <input (input)="query.set($any($event.target).value)" type="search" placeholder="Search..." />
+    </fieldset>
+    @if (users.isLoading()) {
+      <mat-progress-bar mode="query" />
+    }
+    @if (users.error()) {
+      <div class="error">{{ users.error() }}</div>
+    }
+    <section class="actions">
+      <button (click)="users.reload()">Reload</button>
+      <button (click)="addUser()">Add User</button>
+      <button (click)="users.set([])">Clear</button>
+    </section>
+    <ul>
+      @for (user of users.value(); track user.id) {
+        <li>{{ user.name }}</li>
+      } @empty {
+        <li class="no-data">Nothing to show</li>
+      }
+    </ul>
+  `,
+  styleUrl: 'user-search.component.scss',
+})
+export class UserSearchComponent {
+  query = signal('');
+  users = resource<User[], { query: string }>({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request, abortSignal }) => {
+      const users = await fetch(`${API_URL}?name_like=^${request.query}`, {
+        signal: abortSignal,
+      });
+      if (!users.ok) throw Error(`Could not fetch...`);
+      return await users.json();
+    },
+  });
+  constructor() {}
+  addUser() {
+    const user = { id: 123, name: 'Dmytro Mezhenskyi' };
+    this.users.update((users) => (users ? [user, ...users] : [user]));
+  }
+}
