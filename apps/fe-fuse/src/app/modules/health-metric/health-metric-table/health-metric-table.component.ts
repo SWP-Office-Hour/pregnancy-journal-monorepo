26c4e1db916aeaf6f2c96ffcea086aed40711659
nnh53 <nnh53.work@gmail.com>
2025-02-17 21:06:25 +0700

    feat: be fix for me so i can login pls

--- apps/fe-fuse/src/app/modules/health-metric/health-metric-table/health-metric-table.component.ts
+++ apps/fe-fuse/src/app/modules/health-metric/health-metric-table/health-metric-table.component.ts
@@ -1,6 +1,6 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, OnInit, resource, signal } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, UntypedFormControl } from '@angular/forms';
+import { ChangeDetectorRef, Component, resource, signal } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatOptionModule, MatRippleModule } from '@angular/material/core';
@@ -14,6 +14,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSortModule } from '@angular/material/sort';
 import { fuseAnimations } from '@fuse/animations';
 import { HealthMetric, Status } from '@pregnancy-journal-monorepo/contract';
+import { FuseConfirmationService } from '../../../../../../fuse-example/src/@fuse/services/confirmation';
 import { environment } from '../../../../environments/environment';
 
 @Component({
@@ -40,11 +41,16 @@ import { environment } from '../../../../environments/environment';
     NgTemplateOutlet,
   ],
 })
-export class HealthMetricTableComponent implements OnInit {
+export class HealthMetricTableComponent {
+  protected readonly Status = Status;
+  flashMessage: 'success' | 'error' | null = null;
   isLoading: boolean = false;
   selectedMetric: HealthMetric | null = null;
+  selectedMetricForm: UntypedFormGroup;
   searchInputControl: UntypedFormControl = new UntypedFormControl();
+
   metricList = signal<Array<HealthMetric>>([]);
+
   metricResource = resource<HealthMetric[], {}>({
     loader: async ({ abortSignal }) => {
       const response = await fetch(environment.apiUrl + 'metrics', {
@@ -55,26 +61,30 @@ export class HealthMetricTableComponent implements OnInit {
     },
   });
 
-  constructor() {}
-
-  async ngOnInit() {
-    console.log('metricResource');
-    console.log(this.metricResource.value());
-    console.log('metricList signal');
-    console.log(this.metricList());
-    console.log('update signal');
-    console.log(this.metricResource.value());
-    const data = this.metricResource.value();
-    this.metricList.set(data!);
-    console.log('metricList signal');
-    console.log(this.metricList());
+  /**
+   * Constructor
+   */
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _fuseConfirmationService: FuseConfirmationService,
+    private _changeDetectorRef: ChangeDetectorRef,
+  ) {
+    // Create the selected product form
+    this.selectedMetricForm = this._formBuilder.group({
+      metric_id: [''],
+      title: [''],
+      measurement_unit: ['', [Validators.required]],
+      status: Status,
+      required: [false],
+      upperbound_msg: [''],
+      lowerbound_msg: [''],
+      tag: [''],
+    });
+    // effect(() => {
+    //   console.log('metricResource');
+    //   console.log(this.metricResource.value());
+    // });
   }
-
-  createMetric() {
-    console.log('Create metric');
-  }
-
-  protected readonly Status = Status;
 
   toggleDetails(metricId: string): void {
     // If the metric is already selected...
@@ -87,5 +97,55 @@ export class HealthMetricTableComponent implements OnInit {
 
   closeDetails(): void {
     this.selectedMetric = null;
+  }
+
+  createMetric() {
+    console.log('Create metric');
+  }
+
+  deleteSelectedProduct(): void {
+    // Open the confirmation dialog
+    const confirmation = this._fuseConfirmationService.open({
+      title: 'Delete product',
+      message: 'Are you sure you want to remove this product? This action cannot be undone!',
+      actions: {
+        confirm: {
+          label: 'Delete',
+        },
+      },
+    });
+  }
+
+  updateSelectedProduct(): void {
+    // Get the product object
+    const product = this.selectedMetricForm.getRawValue();
+
+    // Remove the currentImageIndex field
+    delete product.currentImageIndex;
+
+    // Update the product on the server
+    // this._inventoryService.updateProduct(product.id, product).subscribe(() => {
+    //   // Show a success message
+    //   this.showFlashMessage('success');
+    // });
+  }
+
+  /**
+   * Show flash message
+   */
+  showFlashMessage(type: 'success' | 'error'): void {
+    // Show the message
+    this.flashMessage = type;
+
+    // Mark for check
+    this._changeDetectorRef.markForCheck();
+
+    // Hide it after 3 seconds
+    setTimeout(() => {
+      this.flashMessage = null;
+
+      // Mark for check
+      this._changeDetectorRef.markForCheck();
+    }, 3000);
   }
 }
