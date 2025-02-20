cc82f8a06cad19e25eaacba09cf3a6eca4c608b2
ThanhTNV <thanhtran190304@gmail.com>
2025-02-20 21:41:18 +0700

    chore: create blob util to convert base64 to blob

---
+++ apps/fe-fuse/src/app/modules/customer/common/blob.utils.ts
@@ -0,0 +1,21 @@
+export function dataURItoBlob(dataURI: string): Blob {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  const byteString = dataURI.split(',')[0].indexOf('base64') >= 0 ? atob(dataURI.split(',')[1]) : unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  const ia = new Uint8Array(byteString.length);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], { type: mimeString });
+}
+
+export function getFormData(blob: Blob) {
+  const fd = new FormData(document.forms[0]);
+  fd.append('file', blob);
+  return fd;
+}
