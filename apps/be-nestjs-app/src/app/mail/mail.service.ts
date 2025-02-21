3f893e0ed7faa4cce26e7bb3e0e6c4b54055e699
phatcanlez <phatcanlez@gmail.com>
2025-02-21 10:06:34 +0700

    feat: thêm template và service

--- apps/be-nestjs-app/src/app/mail/mail.service.ts
+++ apps/be-nestjs-app/src/app/mail/mail.service.ts
@@ -1,28 +1,33 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class MailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly userService: UsersService,
+  ) {}
 
-  public send(): void {
-    this.mailerService
-      .sendMail({
-        to: 'phatcanlez@gmail.com', // list of receivers
-        from: 'noreply@gmail.com', // sender address
-        subject: 'Testing Nest MailerModule ✔', // Subject line
-        text: 'welcome', // plaintext body
-        template: 'register',
-        context: {
-          name: 'phatcanlez',
-          activationCode: '123456',
-        },
-      })
-      .then(() => {
-        console.log('Email sent');
-      })
-      .catch(() => {
-        console.log('Error sending email');
-      });
+  async resetPassword(userId: string) {
+    const user = await this.userService.getUserById(userId);
+
+    const uuid = uuidv4().replace(/[^0-9]/g, '');
+    const code = parseInt(uuid.slice(0, 6), 10);
+
+    await this.mailerService.sendMail({
+      to: 'hoangyen7592sg@gmail.com', // list of receivers
+      from: 'noreply@gmail.com', // sender address
+      subject: 'Reset Password', // Subject line
+      text: 'reset password', // plaintext body
+      template: 'reset-password',
+      context: {
+        name: user.name,
+        resetCode: code,
+      },
+    });
+
+    return code;
   }
 }
