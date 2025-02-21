04edd1cf4965b49b4c78f03d5417c98b802d53db
nnh53 <nnh53.work@gmail.com>
2025-02-21 22:52:17 +0700

    feat: clean-up-fuse

--- apps/fe-fuse/src/app/mock-api/index.ts
+++ apps/fe-fuse/src/app/mock-api/index.ts
@@ -1,37 +1,25 @@
-import { AcademyMockApi } from 'app/mock-api/apps/academy/api';
-import { ChatMockApi } from 'app/mock-api/apps/chat/api';
-import { ContactsMockApi } from 'app/mock-api/apps/contacts/api';
 import { AuthMockApi } from 'app/mock-api/common/auth/api';
-import { MessagesMockApi } from 'app/mock-api/common/messages/api';
 import { NavigationMockApi } from 'app/mock-api/common/navigation/api';
 import { NotificationsMockApi } from 'app/mock-api/common/notifications/api';
-// import { SearchMockApi } from 'app/mock-api/common/search/api';
-import { ShortcutsMockApi } from 'app/mock-api/common/shortcuts/api';
 import { UserMockApi } from 'app/mock-api/common/user/api';
-import { AnalyticsMockApi } from 'app/mock-api/dashboards/analytics/api';
-import { CryptoMockApi } from 'app/mock-api/dashboards/crypto/api';
-import { FinanceMockApi } from 'app/mock-api/dashboards/finance/api';
-import { ProjectMockApi } from 'app/mock-api/dashboards/project/api';
-import { ActivitiesMockApi } from 'app/mock-api/pages/activities/api';
-import { IconsMockApi } from 'app/mock-api/ui/icons/api';
-import { BlogsMockApi } from './dashboards/blogs/api';
+import { IconsMockApi } from 'app/mock-api/icons/api';
 
 export const mockApiServices = [
-  AcademyMockApi,
-  ActivitiesMockApi,
-  AnalyticsMockApi,
+  // AcademyMockApi,
+  // ActivitiesMockApi,
+  // AnalyticsMockApi,
   AuthMockApi,
-  ChatMockApi,
-  ContactsMockApi,
-  CryptoMockApi,
-  FinanceMockApi,
+  // ChatMockApi,
+  // ContactsMockApi,
+  // CryptoMockApi,
+  // FinanceMockApi,
   IconsMockApi,
-  MessagesMockApi,
+  // MessagesMockApi,
   NavigationMockApi,
   NotificationsMockApi,
-  ProjectMockApi,
+  // ProjectMockApi,
   // SearchMockApi,
-  ShortcutsMockApi,
+  // ShortcutsMockApi,
   UserMockApi,
-  BlogsMockApi,
+  // BlogsMockApi,
 ];
