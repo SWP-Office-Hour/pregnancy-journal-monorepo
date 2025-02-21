b1f95d8f826b642db6390a1a014e2ec759332356
nnh53 <nnh53.work@gmail.com>
2025-02-21 12:22:59 +0700

    feat: make shiet better to look at

--- apps/fe-fuse/src/environments/environment.development.ts
+++ apps/fe-fuse/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/',
-  // apiUrl: 'https://pregnancy-journal-swp-gduen.ondigitalocean.app/',
+  // apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://pregnancy-journal-swp-gduen.ondigitalocean.app/',
   clientId: '616565294967-v2e8j0mi27a6ppbpt00mj7ge2dc2ninj.apps.googleusercontent.com',
 };
