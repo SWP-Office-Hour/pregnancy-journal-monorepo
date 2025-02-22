20013b55621622a3d2bf2f7ab117718cd4c7eebe
phatcanlez <phatcanlez@gmail.com>
2025-02-22 08:06:11 +0700

    fix: fix bug reminder module

--- apps/be-nestjs-app/src/app/reminder/reminder.module.ts
+++ apps/be-nestjs-app/src/app/reminder/reminder.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
 import { UsersModule } from '../users/users.module';
+import { JwtUtilsModule } from '../utils/jwt/jwtUtils.module';
 import { ReminderController } from './reminder.controller';
 import { ReminderService } from './reminder.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtUtilsModule],
   controllers: [ReminderController],
   providers: [ReminderService, DatabaseService],
 })
