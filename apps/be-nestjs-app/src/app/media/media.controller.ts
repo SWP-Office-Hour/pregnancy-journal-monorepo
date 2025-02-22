cadd976eb9278d63c514f6c9c340ad60c34b2e45
phatcanlez <phatcanlez@gmail.com>
2025-02-22 20:06:59 +0700

    fix: tạo media giống của thành và chỉnh contract

--- apps/be-nestjs-app/src/app/media/media.controller.ts
+++ apps/be-nestjs-app/src/app/media/media.controller.ts
@@ -1,9 +1,6 @@
-import { BadRequestException, Body, Controller, NotFoundException, Param, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { BadRequestException, Body, Controller, NotFoundException, Param, Query } from '@nestjs/common';
 import { mediaContract, MediaResponse } from '@pregnancy-journal-monorepo/contract';
 import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
-import { Express } from 'express';
-import { unlink } from 'fs/promises';
 import { FileService } from '../file/file.service';
 import { MediaService } from './media.service';
 
@@ -15,17 +12,7 @@ export class MediaController {
   ) {}
 
   @TsRestHandler(mediaContract.updateMultiFiles)
-  uploadFile(
-    @Body() media: MediaResponse[],
-    @Query()
-    {
-      post_id,
-      record_id,
-    }: {
-      post_id?: string;
-      record_id?: string;
-    },
-  ) {
+  uploadFile(@Body() media: MediaResponse[], @Query() { post_id, record_id }: { post_id?: string; record_id?: string }) {
     return tsRestHandler(mediaContract.updateMultiFiles, async () => {
       if (!post_id && !record_id) {
         throw new BadRequestException('No post_id or record_id provided');
@@ -124,59 +111,137 @@ export class MediaController {
   }
 
   @TsRestHandler(mediaContract.updateFile)
-  @UseInterceptors(FileInterceptor('file'))
-  handleUpdateFile(
-    @UploadedFile() file: Express.Multer.File,
-    @Query()
-    {
-      post_id,
-      record_id,
-    }: {
-      post_id?: string;
-      record_id?: string;
-    },
-  ) {
-    return tsRestHandler(mediaContract.updateFile, async () => {
+  handleUploadFile(@Body() media: MediaResponse[], @Query() { post_id, record_id }: { post_id?: string; record_id?: string }) {
+    return tsRestHandler(mediaContract.updateMultiFiles, async () => {
       if (!post_id && !record_id) {
         throw new BadRequestException('No post_id or record_id provided');
       }
 
-      if (!file) {
-        throw new BadRequestException('No file uploaded');
+      if (!media) {
+        throw new BadRequestException('No media uploaded');
       }
 
-      const uniqueName = Date.now() + '-' + file.originalname;
-      try {
-        const upload = await this.fileService.uploadToR2(file, uniqueName);
-        if (!upload) {
+      const media_urls = await Promise.all(media.map((m) => fetch(m.media_url)));
+      const blobs = (await Promise.all(media_urls.map((m) => m.blob()))).map((blob) => {
+        return {
+          buffer: blob,
+          filename: Date.now() + '-blob',
+        };
+      });
+
+      const result: {
+        post_id: string | null;
+        media_id: string;
+        media_url: string;
+        created_at: Date;
+        visit_record_id: string | null;
+      }[] = [];
+
+      // IF UPDATE MEDIA HAVE NEW MEDIA
+      if (blobs) {
+        const uploads = await Promise.all(
+          blobs.map((blob) => {
+            return this.fileService.uploadToR2Blob(blob.buffer, blob.filename);
+          }),
+        );
+        if (!uploads) {
           throw new BadRequestException('Error uploading file');
         }
-        let result: MediaResponse;
+
+        if (uploads.length !== blobs.length) {
+          throw new BadRequestException('Error uploading file with upload count not match');
+        }
+
+        if (uploads.some((upload) => !upload)) {
+          throw new BadRequestException('Error uploading file with some upload failed');
+        }
 
         if (post_id) {
-          result = await this.mediaService.createWithPostId({ media_url: uniqueName, post_id });
+          const create_result = await Promise.all(
+            blobs.map((blob) =>
+              this.mediaService.createWithPostId({
+                media_url: blob.filename,
+                post_id,
+              }),
+            ),
+          );
+          result.push(...create_result);
         } else if (record_id) {
-          result = await this.mediaService.createWithRecordId({ media_url: uniqueName, record_id });
+          const create_result = await Promise.all(
+            blobs.map((blob) =>
+              this.mediaService.createWithRecordId({
+                media_url: blob.filename,
+                record_id,
+              }),
+            ),
+          );
+          result.push(...create_result);
         } else {
           throw new BadRequestException('No post_id or record_id provided');
         }
-
-        console.log(file);
-        if (file.path) await unlink(file.path);
-        //xóa file sau khi upload
-
-        return {
-          status: 200 as const,
-          body: result,
-        };
-      } catch (error) {
-        if (file.path) {
-          await unlink(file.path); // Xóa file tạm nếu upload thất bại
-        }
-        throw new BadRequestException(error.message);
       }
+
+      return {
+        status: 200 as const,
+        body: result,
+      };
     });
   }
+
+  // @TsRestHandler(mediaContract.updateFile)
+  // @UseInterceptors(FileInterceptor('file'))
+  // handleUpdateFile(
+  //   @UploadedFile() file: Express.Multer.File,
+  //   @Query()
+  //   {
+  //     post_id,
+  //     record_id,
+  //   }: {
+  //     post_id?: string;
+  //     record_id?: string;
+  //   },
+  // ) {
+  //   return tsRestHandler(mediaContract.updateFile, async () => {
+  //     if (!post_id && !record_id) {
+  //       throw new BadRequestException('No post_id or record_id provided');
+  //     }
+  //
+  //     if (!file) {
+  //       throw new BadRequestException('No file uploaded');
+  //     }
+  //
+  //     const uniqueName = Date.now() + '-' + file.originalname;
+  //     try {
+  //       const upload = await this.fileService.uploadToR2(file, uniqueName);
+  //       if (!upload) {
+  //         throw new BadRequestException('Error uploading file');
+  //       }
+  //       let result: MediaResponse;
+  //
+  //       if (post_id) {
+  //         result = await this.mediaService.createWithPostId({ media_url: uniqueName, post_id });
+  //       } else if (record_id) {
+  //         result = await this.mediaService.createWithRecordId({ media_url: uniqueName, record_id });
+  //       } else {
+  //         throw new BadRequestException('No post_id or record_id provided');
+  //       }
+  //
+  //       console.log(file);
+  //       if (file.path) await unlink(file.path);
+  //       //xóa file sau khi upload
+  //
+  //       return {
+  //         status: 200 as const,
+  //         body: result,
+  //       };
+  //     } catch (error) {
+  //       if (file.path) {
+  //         await unlink(file.path); // Xóa file tạm nếu upload thất bại
+  //       }
+  //       throw new BadRequestException(error.message);
+  //     }
+  //   });
+  // }
 
   // @TsRestHandler(mediaContract.getFile)
   // handleGetFile(@Param('filename') filename: string, @Res() res) {
