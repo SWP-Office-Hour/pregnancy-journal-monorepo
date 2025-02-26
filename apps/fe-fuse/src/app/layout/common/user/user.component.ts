8215769599deb1eff73815492ddc42ec1f548e04
ThanhTNV <thanhtran190304@gmail.com>
2025-02-26 17:15:45 +0700

    fix: user profile navigation

--- apps/fe-fuse/src/app/layout/common/user/user.component.ts
+++ apps/fe-fuse/src/app/layout/common/user/user.component.ts
@@ -94,4 +94,8 @@ export class UserComponent implements OnInit, OnDestroy {
   signOut(): void {
     this._router.navigate(['/sign-out']);
   }
+
+  profile() {
+    this._router.navigate(['/user-profile']);
+  }
 }
