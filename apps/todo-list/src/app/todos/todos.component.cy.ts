213d58f8335bc6dcc296c051937b2bd31110fcad
ThanhTNV <thanhtran190304@gmail.com>
2025-03-03 16:00:50 +0700

    feat: test add todo item, select todo item

--- apps/todo-list/src/app/todos/todos.component.cy.ts
+++ apps/todo-list/src/app/todos/todos.component.cy.ts
@@ -14,4 +14,22 @@ describe(TodosComponent.name, () => {
   it('renders', () => {
     cy.mount(TodosComponent);
   });
+
+  it('should includes todo items', () => {
+    cy.mount(TodosComponent);
+    cy.get('.todos > app-todo-item').should('exist');
+  });
+
+  it('should add todo item', () => {
+    cy.mount(TodosComponent);
+    cy.get('#new_todo').type('New todo item');
+    cy.get('#new_todo_btn').click();
+    cy.get('app-todo-item').children().first().children('label').should('contain.html', 'New todo item'.toUpperCase());
+  });
+
+  it('should update todo item', () => {
+    cy.mount(TodosComponent);
+    cy.get('app-todo-item').children().first().children('input').click();
+    cy.get('app-todo-item').children().first().should('have.css', 'text-decoration', 'line-through solid rgb(108, 117, 125)');
+  });
 });
