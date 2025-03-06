2c36f9ca5e474f6127a02de7c7687504041f63f5
phatcanlez <phatcanlez@gmail.com>
2025-03-06 21:46:49 +0700

    feat: thêm admin controller và service

--- apps/be-nestjs-app/src/app/app.module.ts
+++ apps/be-nestjs-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
@@ -47,6 +48,7 @@ import { UsersModule } from './users/users.module';
     MailModule,
     PayosModule,
     PaymentModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
