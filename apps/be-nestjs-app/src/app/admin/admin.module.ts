188e237a33e825db38a79dd3455b222654153195
phatcanlez <phatcanlez@gmail.com>
2025-03-07 21:48:35 +0700

    fix: bug

--- apps/be-nestjs-app/src/app/admin/admin.module.ts
+++ apps/be-nestjs-app/src/app/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, DatabaseService],
 })
 export class AdminModule {}
