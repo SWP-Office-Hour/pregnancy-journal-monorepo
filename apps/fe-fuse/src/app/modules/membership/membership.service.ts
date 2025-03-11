ecbec30956ba6cc7793add4b1067fc63549715b3
phatcanlez <phatcanlez@gmail.com>
2025-03-11 12:44:08 +0700

    fix: bug FE update gọi api

--- apps/fe-fuse/src/app/modules/membership/membership.service.ts
+++ apps/fe-fuse/src/app/modules/membership/membership.service.ts
@@ -42,7 +42,7 @@ export class membershipService {
   async updatePayment(payment: PaymentUpdateRequestType) {
     console.log('gọi hàm update payment status');
     const response = await fetch(`${environment.apiUrl}payments`, {
-      method: 'POST',
+      method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${this._authService.accessToken}`,
