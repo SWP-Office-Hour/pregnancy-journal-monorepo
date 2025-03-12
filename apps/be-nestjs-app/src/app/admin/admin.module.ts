0f0c231cdc32613f8abd7569d8dc3df5e459e7d8
phatcanlez <phatcanlez@gmail.com>
2025-03-12 11:44:32 +0700

    feat: api get dashboard

--- apps/be-nestjs-app/src/app/admin/admin.module.ts
+++ apps/be-nestjs-app/src/app/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
+import { UsersModule } from '../users/users.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AdminController],
   providers: [AdminService, DatabaseService],
 })
