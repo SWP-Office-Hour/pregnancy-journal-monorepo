9e29f4a3eeb92a5d5a60255722be5cbc39f26897
ThanhTNV <thanhtran190304@gmail.com>
2025-03-12 19:35:52 +0700

    feat: add system remind message by week

--- apps/fe-fuse/src/app/modules/member/home/home.component.ts
+++ apps/fe-fuse/src/app/modules/member/home/home.component.ts
@@ -1,7 +1,7 @@
 import { TextFieldModule } from '@angular/cdk/text-field';
 import { NgClass } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -31,6 +31,7 @@ import { FuseCardComponent } from '../../../../@fuse/components/card';
 import { environment } from '../../../../environments/environment';
 import { UserService } from '../../../core/user/user.service';
 import { User } from '../../../core/user/user.types';
+import { SystemReminder, SystemReminders } from '../../../mock-api/system-remind.data';
 import { PregnancyRecordService } from '../../customer/pregnancy-record/pregnancy-record.service';
 import { HomeReminderComponent } from '../home-reminder/home-reminder.component';
 import { RecommendedBlogsComponent } from '../recommended-blogs/recommended-blogs.component';
@@ -77,92 +78,7 @@ import { RecommendedBlogsComponent } from '../recommended-blogs/recommended-blog
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  //Thai nhi
-
-  weekData = {
-    '0001': { dueDate: '280', dueDateFormatted: '01/01/2025', size: '0.1mm', sizeDescription: 'Bằng hạt anh túc' },
-    '0002': { dueDate: '273', dueDateFormatted: '02/01/2025', size: '0.2mm', sizeDescription: 'Bằng hạt cải' },
-    '0003': { dueDate: '266', dueDateFormatted: '03/01/2025', size: '0.4mm', sizeDescription: 'Bằng hạt vừng' },
-    '0004': { dueDate: '259', dueDateFormatted: '04/01/2025', size: '0.8mm', sizeDescription: 'Bằng hạt gạo' },
-    '0005': { dueDate: '252', dueDateFormatted: '05/01/2025', size: '2mm', sizeDescription: 'Bằng hạt đậu' },
-    '0006': { dueDate: '245', dueDateFormatted: '06/01/2025', size: '4mm', sizeDescription: 'Bằng hạt đậu xanh' },
-    '0007': { dueDate: '238', dueDateFormatted: '07/01/2025', size: '8mm', sizeDescription: 'Bằng quả nho' },
-    '0008': { dueDate: '231', dueDateFormatted: '08/01/2025', size: '16mm', sizeDescription: 'Bằng quả dâu' },
-    '0009': { dueDate: '224', dueDateFormatted: '09/01/2025', size: '23mm', sizeDescription: 'Bằng quả ô liu' },
-    '0010': { dueDate: '217', dueDateFormatted: '10/01/2025', size: '30mm', sizeDescription: 'Bằng quả mận' },
-    '0011': { dueDate: '210', dueDateFormatted: '11/01/2025', size: '38mm', sizeDescription: 'Bằng quả sung' },
-    '0012': { dueDate: '203', dueDateFormatted: '12/01/2025', size: '45mm', sizeDescription: 'Bằng quả chanh' },
-    '0013': { dueDate: '196', dueDateFormatted: '13/01/2025', size: '55mm', sizeDescription: 'Bằng quả đào' },
-    '0014': { dueDate: '189', dueDateFormatted: '14/01/2025', size: '65mm', sizeDescription: 'Bằng quả lê' },
-    '0015': { dueDate: '182', dueDateFormatted: '15/01/2025', size: '75mm', sizeDescription: 'Bằng quả táo' },
-    '0016': { dueDate: '175', dueDateFormatted: '16/01/2025', size: '85mm', sizeDescription: 'Bằng quả bơ' },
-    '0017': { dueDate: '168', dueDateFormatted: '17/01/2025', size: '95mm', sizeDescription: 'Bằng quả cam' },
-    '0018': { dueDate: '161', dueDateFormatted: '18/01/2025', size: '105mm', sizeDescription: 'Bằng quả xoài' },
-    '0019': { dueDate: '154', dueDateFormatted: '19/01/2025', size: '115mm', sizeDescription: 'Bằng quả đu đủ' },
-    '0020': { dueDate: '147', dueDateFormatted: '20/01/2025', size: '125mm', sizeDescription: 'Bằng quả dừa' },
-    '0021': { dueDate: '140', dueDateFormatted: '21/01/2025', size: '135mm', sizeDescription: 'Bằng quả dưa hấu nhỏ' },
-    '0022': { dueDate: '133', dueDateFormatted: '22/01/2025', size: '145mm', sizeDescription: 'Bằng quả dưa gang' },
-    '0023': { dueDate: '126', dueDateFormatted: '23/01/2025', size: '155mm', sizeDescription: 'Bằng quả bưởi' },
-    '0024': { dueDate: '119', dueDateFormatted: '24/01/2025', size: '165mm', sizeDescription: 'Bằng quả mít' },
-    '0025': { dueDate: '112', dueDateFormatted: '25/01/2025', size: '175mm', sizeDescription: 'Bằng quả dừa xiêm' },
-    '0026': { dueDate: '105', dueDateFormatted: '26/01/2025', size: '185mm', sizeDescription: 'Bằng quả dưa hấu' },
-    '0027': { dueDate: '98', dueDateFormatted: '27/01/2025', size: '195mm', sizeDescription: 'Bằng bắp cải' },
-    '0028': { dueDate: '91', dueDateFormatted: '28/01/2025', size: '205mm', sizeDescription: 'Bằng quả bí đỏ nhỏ' },
-    '0029': { dueDate: '84', dueDateFormatted: '29/01/2025', size: '215mm', sizeDescription: 'Bằng quả bí đỏ' },
-    '0030': { dueDate: '77', dueDateFormatted: '30/01/2025', size: '225mm', sizeDescription: 'Bằng quả bí ngô' },
-    '0031': { dueDate: '70', dueDateFormatted: '31/01/2025', size: '235mm', sizeDescription: 'Bằng quả bầu' },
-    '0032': { dueDate: '63', dueDateFormatted: '01/02/2025', size: '245mm', sizeDescription: 'Bằng quả mướp' },
-    '0033': { dueDate: '56', dueDateFormatted: '02/02/2025', size: '255mm', sizeDescription: 'Bằng quả su su' },
-    '0034': { dueDate: '49', dueDateFormatted: '03/02/2025', size: '265mm', sizeDescription: 'Bằng quả đu đủ lớn' },
-    '0035': { dueDate: '42', dueDateFormatted: '04/02/2025', size: '275mm', sizeDescription: 'Bằng quả dưa hấu lớn' },
-    '0036': { dueDate: '35', dueDateFormatted: '05/02/2025', size: '285mm', sizeDescription: 'Bằng quả bí đao' },
-    '0037': { dueDate: '28', dueDateFormatted: '06/02/2025', size: '295mm', sizeDescription: 'Bằng quả bí rợ' },
-    '0038': { dueDate: '21', dueDateFormatted: '07/02/2025', size: '305mm', sizeDescription: 'Bằng quả mít to' },
-    '0039': { dueDate: '14', dueDateFormatted: '08/02/2025', size: '315mm', sizeDescription: 'Bằng quả bí đỏ to' },
-    '0040': { dueDate: '7', dueDateFormatted: '09/02/2025', size: '325mm', sizeDescription: 'Bằng em bé sơ sinh' },
-  };
-  standardResource = [
-    { week: 1, weight: 0 },
-    { week: 2, weight: 0 },
-    { week: 3, weight: 0 },
-    { week: 4, weight: 0 },
-    { week: 5, weight: 0 },
-    { week: 6, weight: 0 },
-    { week: 7, weight: 0 },
-    { week: 8, weight: 1 },
-    { week: 9, weight: 2 },
-    { week: 10, weight: 4 },
-    { week: 11, weight: 7 },
-    { week: 12, weight: 14 },
-    { week: 13, weight: 23 },
-    { week: 14, weight: 43 },
-    { week: 15, weight: 70 },
-    { week: 16, weight: 100 },
-    { week: 17, weight: 140 },
-    { week: 18, weight: 190 },
-    { week: 19, weight: 240 },
-    { week: 20, weight: 300 },
-    { week: 21, weight: 360 },
-    { week: 22, weight: 430 },
-    { week: 23, weight: 501 },
-    { week: 24, weight: 600 },
-    { week: 25, weight: 660 },
-    { week: 26, weight: 760 },
-    { week: 27, weight: 875 },
-    { week: 28, weight: 1005 },
-    { week: 29, weight: 1153 },
-    { week: 30, weight: 1319 },
-    { week: 31, weight: 1502 },
-    { week: 32, weight: 1702 },
-    { week: 33, weight: 1918 },
-    { week: 34, weight: 2146 },
-    { week: 35, weight: 2383 },
-    { week: 36, weight: 2622 },
-    { week: 37, weight: 2859 },
-    { week: 38, weight: 3083 },
-    { week: 39, weight: 3288 },
-    { week: 40, weight: 3290 },
-  ];
+  public systemRemind: WritableSignal<SystemReminder> = null;
   public formatDate: string;
   countDownForm = new FormGroup({
     lastMenstrualPeriod: new FormControl('', Validators.required),
@@ -192,6 +108,7 @@ export class HomeComponent {
         this._expectedDate = new Date(user.expected_birth_date) || new Date();
         this.calculateCurrentPregnancyWeek();
         this._countWeek = this._currentPregnancyWeek;
+        this.systemRemind.set(SystemReminders.find((item) => item.week === this._currentPregnancyWeek) || null);
       }
     });
     this._recordService.getMetrics().subscribe((metrics) => {
@@ -216,6 +133,7 @@ export class HomeComponent {
         }
       });
   }
+
   //chưa có data bên database
   // standardResource = resource<Standard[], {}>({
   //   loader: async ({ abortSignal }) => {
