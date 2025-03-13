86f92fa5b2b7016f06f79fa488d9f139bcc5dad5
phatcanlez <phatcanlez@gmail.com>
2025-03-13 15:02:12 +0700

    feat: api child

--- apps/be-nestjs-app/src/app/child/child.module.ts
+++ apps/be-nestjs-app/src/app/child/child.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
 import { UsersModule } from '../users/users.module';
+import { JwtUtilsModule } from '../utils/jwt/jwtUtils.module';
 import { ChildController } from './child.controller';
 import { ChildService } from './child.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtUtilsModule],
   controllers: [ChildController],
   providers: [ChildService, DatabaseService],
   exports: [ChildService],
--- apps/be-nestjs-app/src/app/child/child.service.ts
+++ apps/be-nestjs-app/src/app/child/child.service.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { ChildCreateRequestType } from '../../../../../libs/contract/src/lib/child.contract';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ChildCreateRequestType, ChildUpdateRequestType } from '../../../../../libs/contract/src/lib/child.contract';
 import { DatabaseService } from '../database/database.service';
+import { Child } from './entities/child.entity';
 
 @Injectable()
 export class ChildService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async getChildById(child_id: string) {
-    return await this.databaseService.Child.findUnique({ where: { child_id } });
+  async getChildById(child_id: string): Promise<Child> {
+    const child = await this.databaseService.Child.findUnique({ where: { child_id } });
+    if (!child) {
+      throw new NotFoundException('Child not found');
+    }
+    return child;
   }
 
-  async getAllChildren(user_id: string) {
+  async getAllChildren(user_id: string): Promise<Child[]> {
     return await this.databaseService.Child.findMany({
       where: {
         user_id: user_id,
@@ -27,6 +32,37 @@ export class ChildService {
           },
         },
         ...createChild,
+      },
+    });
+  }
+
+  async updateChild(updateChild: ChildUpdateRequestType) {
+    return await this.databaseService.Child.update({
+      where: {
+        child_id: updateChild.child_id,
+      },
+      data: updateChild,
+    });
+  }
+
+  async deleteChild(child_id: string) {
+    const deleteRecord = await this.databaseService.Record.findMany({
+      where: {
+        child_id,
+      },
+    });
+
+    if (deleteRecord.length > 0) {
+      await this.databaseService.Record.deleteMany({
+        where: {
+          child_id,
+        },
+      });
+    }
+
+    return await this.databaseService.Child.delete({
+      where: {
+        child_id,
       },
     });
   }
