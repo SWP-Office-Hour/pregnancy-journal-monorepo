25f668ecdf7ff3a3d626158950bcd4f00ea7fbfe
phatcanlez <phatcanlez@gmail.com>
2025-03-28 16:04:51 +0700

    fix: fix bug api tạo thêm reminder

--- apps/be-nestjs-app/src/app/child/child.module.ts
+++ apps/be-nestjs-app/src/app/child/child.module.ts
@@ -2,15 +2,16 @@ import { forwardRef, Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { RecordsModule } from '../records/records.module';
 import { ReminderModule } from '../reminder/reminder.module';
+import { ReminderService } from '../reminder/reminder.service';
 import { UsersModule } from '../users/users.module';
 import { JwtUtilsModule } from '../utils/jwt/jwtUtils.module';
 import { ChildController } from './child.controller';
 import { ChildService } from './child.service';
 
 @Module({
-  imports: [forwardRef(() => UsersModule), JwtUtilsModule, RecordsModule, DatabaseModule, ReminderModule],
+  imports: [ReminderModule, forwardRef(() => UsersModule), JwtUtilsModule, RecordsModule, DatabaseModule],
   controllers: [ChildController],
-  providers: [ChildService],
+  providers: [ChildService, ReminderService],
   exports: [ChildService],
 })
 export class ChildModule {}
